d63e4acaf50e5a2788cd3bf9094068d024af72b9
viktorsuster <viktor.suster.mail@gmail.com>
2021-05-13 10:03:56 +0200

    Set up tooling

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Box, Center, Heading, Button } from "@chakra-ui/react";
+import bg from "../assets/bg.jpg";
 
 export const Home = () => (
-    <h1>Homepage</h1>
-)
+  <Box background={`url('${bg}') center / cover no-repeat`} h="100vh">
+    <Center h="100vh">
+      <div>
+        <Heading color="white" textAlign="center" mb="4">
+          Welcome!
+        </Heading>
+        <Button>Ready?</Button>
+      </div>
+    </Center>
+  </Box>
+);
