313ee4d69d6583ce7fd79746e148a2c93d9b4a4a
viktorsuster <viktor.suster.mail@gmail.com>
2021-05-13 10:16:15 +0200

    Changes without gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider } from '@chakra-ui/react'
 import { Home } from './pages/home'
 
 export const App = () => (
-<ChakraProvider>
-<Home />
-</ChakraProvider>
+  <ChakraProvider>
+    <Home />
+  </ChakraProvider>
 )
